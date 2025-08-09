eebb71f9623905181abb68cf5d1c3f12b5de6573
alejandro <agarces691@gmail.com>
2025-08-09 17:35:43 -0400

    attach ballons and player

--- scripts/balloon_manager.gd
+++ scripts/balloon_manager.gd
@@ -3,8 +3,8 @@ extends Node2D
 var helium_balloon: PackedScene = preload("res://entities/helium_balloon.tscn")
 
 func _ready():
-	create_balloons(30, 100)#left group
-	create_balloons(220, 290)#right group
+	create_balloons(-35, 35)#left group
+	
 
 func create_balloons(from,to):
 	for i in 30:
@@ -14,11 +14,11 @@ func create_balloons(from,to):
 		var balloon = instance.balloon
 		balloon.origin = randi_range(from,to)
 		balloon.position.x = randi_range(30,100)
-		balloon.position.y = randi_range(20,60)
+		balloon.position.y = randi_range(-70,-100)
 		balloon.speed = 1
 		balloon.ang =randi_range(-1,1) 
 		balloon.radius = randi_range(5,15)
 		
 		var line = instance.line
 		line.x_final = (from + to) / 2
-		line.y_final = 140
+		line.y_final = 20
