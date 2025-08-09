5326ee7a6a491562718c59a4da23fb2ee292533e
Kevin Esaa <kevin.esaa@gmail.com>
2025-08-09 16:43:22 -0400

    change button texture

--- scripts/keypressSprite.gd
+++ scripts/keypressSprite.gd
@@ -7,13 +7,23 @@ var pressKeyTexture : Texture
 var unpressKeyTexture : Texture
 
 var isPress:bool = false
+var minDelta:float = 100
+var counter:float = 0
 
 func _ready() -> void:
 	pressKeyTexture = load(pressButtonPath)
 	unpressKeyTexture = load(unpressButtonPath)
 
-func _process(delta: float) -> void:
-	if(isPress):
+func _physics_process(delta: float) -> void:
+	minDelta = min(minDelta,delta)
+	if(isPress || counter > 0):
 		texture = pressKeyTexture
 	else:
 		texture = unpressKeyTexture
+	counter = counter - minDelta
+
+
+func onButtonPreesListener(myIsPress:bool):
+	self.isPress = myIsPress
+	if(myIsPress):
+		counter = counter + (3 * minDelta)
